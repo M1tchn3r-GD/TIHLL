118f8bfc12d4b6f64a9535decc43515010839a66
M1tchn3r <M1tchn3r.gd@gmail.com>
2026-03-20 16:59:37 -0400

    press alt+f4 to use this

---
+++ TempData.js
@@ -0,0 +1,22 @@
+const TempUserData = {
+
+    temp: [
+        {
+            category: "user settings",
+            items:[
+                setting-1,
+                setting-2,
+                setting-3,
+                setting-4,
+                setting-5
+                // Add more if neeeded
+            ]
+        },
+        {
+            wrs: "World Records",
+            items:[
+                false
+            ]
+        }
+    ]
+}
